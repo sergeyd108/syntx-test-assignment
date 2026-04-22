5bd346fb407e8fcd7ea94ff06f41d3f52ef031dd
Sergey Dorovsky <sergey.dorovsky@drm.la>
2026-04-22 18:26:18 +0300

    build fix

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { watch } from 'vue'
 import { useRouter } from 'vue-router'
+import { useToast } from '@nuxt/ui/composables'
 import ChatBox from '@/features/chat/ChatBox.vue'
 import ChatSkeleton from '@/features/chat/ChatSkeleton.vue'
 import { provideChatMessages } from '@/features/chat/useChatMessages.ts'
